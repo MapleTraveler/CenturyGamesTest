36b78116fffe55055d647fbe35aa4d9644c7ba6b
何雨洋 <13598752812@163.com>
2025-08-26 23:13:50 +0800

    游戏逻辑层，鱼竿逻辑部分

---
+++ Assets/Scripts/GameLogic/FishingRod.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace GameLogic
+{
+    public class FishingRod : MonoBehaviour
+    {
+        // ----- Input -----
+        private InputAction _drag;
+        private InputAction _click;
+        
+        // ----- Components -----
+        private FishHook _fishHook;
+        private FishingLine _fishingLine;
+        private FishSinker _fishSinker;
+        
+        // ----- State -----
+        private bool _hasInit = false;
+        public void Init()
+        {
+            if (_hasInit) return;
+            
+            
+            
+            
+            _hasInit = true;
+        }
+    }
+}
