1305e5d3277fee60af3b2e750e4876e452e5278b
何雨洋 <13598752812@163.com>
2025-08-28 23:55:55 +0800

    创建鱼类数据类

---
+++ Assets/Scripts/Data/FishData/BaseFishData.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Data.FishData
+{
+    [CreateAssetMenu(fileName = "BaseFishData", menuName = "FishingGame/Fish/BaseFish")]
+    public class BaseFishData : ScriptableObject
+    {
+        public string fishName;
+        public string description;
+        public Sprite fishSprite; // 鱼的图片
+        public int fishValue; // 鱼的价值
+        public float fishMoveSpeed; // 鱼的重量
+    }
+}
