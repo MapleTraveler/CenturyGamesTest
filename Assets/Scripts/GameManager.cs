5e71ad1bfc6b1001c754999b787d56a181ef316d
何雨洋 <13598752812@163.com>
2025-08-28 23:58:45 +0800

    完善GameManager逻辑管理

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,39 @@
 ﻿using UnityEngine;
 using System;
+using GameLogic;
+using GameLogic.FishingComponent;
+using GameLogic.Input;
+
 public class GameManager : MonoBehaviour
 {
+    public GlobalConfig globalConfig;
+    public FishRod fishRod;
+    // --- Systems ---
+    private InputFacade _inputFacade;
     
+    private bool _hasInit = false;
+    private void Awake()
+    {
+        Init();
+    }
+
+    private void Start()
+    {
+        _inputFacade.EnableActions();
+    }
+
+    private void Update()
+    {
+        if(!_hasInit) return;
+        _inputFacade.OnUpdate();
+        fishRod.OnUpdate();
+    }
+
+    public void Init()
+    {
+        _inputFacade = new InputFacade(globalConfig.inputActionAsset);
+        fishRod.Init(_inputFacade,globalConfig.fishRodConfig);
+
+        _hasInit = true;
+    }
 }
