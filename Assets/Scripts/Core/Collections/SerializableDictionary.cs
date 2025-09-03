ed1dde7fdfb154a98a6ad0ed4f7f977b01352cc7
何雨洋 <13598752812@163.com>
2025-09-03 20:37:26 +0800

    新增可序列化字典类

---
+++ Assets/Scripts/Core/Collections/SerializableDictionary.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Core.Collections
+{
+    [Serializable]
+    public class SerializableDictionary<TKey, TValue> : Dictionary<TKey, TValue>, ISerializationCallbackReceiver
+    {
+        [SerializeField] private List<TKey> keys = new List<TKey>();
+        [SerializeField] private List<TValue> values = new List<TValue>();
+
+        // 存储数据到序列化列表
+        public void OnBeforeSerialize()
+        {
+            keys.Clear();
+            values.Clear();
+            foreach (var kvp in this)
+            {
+                keys.Add(kvp.Key);
+                values.Add(kvp.Value);
+            }
+        }
+
+        // 从序列化列表恢复数据
+        public void OnAfterDeserialize()
+        {
+            this.Clear();
+            int count = Math.Min(keys.Count, values.Count);
+
+            for (int i = 0; i < count; i++)
+            {
+                if (!this.ContainsKey(keys[i])) // 避免重复键异常
+                {
+                    this[keys[i]] = values[i];
+                }
+                else
+                {
+                    Debug.LogError($"[SerializableDictionary] 检测到重复键: {keys[i]}，该条目已被跳过。");
+                }
+            }
+
+            if (keys.Count != values.Count)
+            {
+                Debug.LogError("[SerializableDictionary] 序列化数据不匹配，键和值的数量不同。");
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Core/Editor/SerializableDictionaryDrawer.cs
@@ -0,0 +1,73 @@
+﻿using Core.Collections;
+using UnityEditor;
+using UnityEngine;
+
+namespace Core.Editor
+{
+    [CustomPropertyDrawer(typeof(SerializableDictionary<,>), true)]
+    public class SerializableDictionaryDrawer : PropertyDrawer
+    {
+        private const float lineHeight = 20f;
+
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            var keysProp = property.FindPropertyRelative("keys");
+            var valuesProp = property.FindPropertyRelative("values");
+
+            EditorGUI.BeginProperty(position, label, property);
+            position.height = lineHeight;
+            property.isExpanded = EditorGUI.Foldout(position, property.isExpanded, label, true);
+
+            if (property.isExpanded)
+            {
+                EditorGUI.indentLevel++;
+                int count = Mathf.Max(keysProp.arraySize, valuesProp.arraySize);
+
+                // 绘制表头
+                position.y += lineHeight;
+                EditorGUI.LabelField(new Rect(position.x, position.y, position.width / 2, lineHeight), "Key");
+                EditorGUI.LabelField(new Rect(position.x + position.width / 2, position.y, position.width / 2, lineHeight), "Value");
+
+                // 绘制元素
+                for (int i = 0; i < count; i++)
+                {
+                    position.y += lineHeight;
+                    if (i >= keysProp.arraySize) keysProp.InsertArrayElementAtIndex(i);
+                    if (i >= valuesProp.arraySize) valuesProp.InsertArrayElementAtIndex(i);
+
+                    var keyProp = keysProp.GetArrayElementAtIndex(i);
+                    var valueProp = valuesProp.GetArrayElementAtIndex(i);
+
+                    EditorGUI.PropertyField(new Rect(position.x, position.y, position.width / 2, lineHeight), keyProp, GUIContent.none);
+                    EditorGUI.PropertyField(new Rect(position.x + position.width / 2, position.y, position.width / 2, lineHeight), valueProp, GUIContent.none);
+                }
+
+                // 添加按钮
+                position.y += lineHeight;
+                if (GUI.Button(new Rect(position.x, position.y, position.width / 2, lineHeight), "Add"))
+                {
+                    keysProp.arraySize++;
+                    valuesProp.arraySize++;
+                }
+                if (GUI.Button(new Rect(position.x + position.width / 2, position.y, position.width / 2, lineHeight), "Remove Last"))
+                {
+                    if (keysProp.arraySize > 0) keysProp.arraySize--;
+                    if (valuesProp.arraySize > 0) valuesProp.arraySize--;
+                }
+
+                EditorGUI.indentLevel--;
+            }
+
+            EditorGUI.EndProperty();
+        }
+
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            if (!property.isExpanded) return lineHeight;
+            var keysProp = property.FindPropertyRelative("keys");
+            var valuesProp = property.FindPropertyRelative("values");
+            int count = Mathf.Max(keysProp.arraySize, valuesProp.arraySize);
+            return lineHeight * (count + 3); // foldout + 表头 + 元素 + 按钮
+        }
+    }
+}
