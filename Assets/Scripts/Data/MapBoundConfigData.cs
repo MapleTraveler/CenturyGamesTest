688d19f6fdcdd08ca1d90c55c32465ef308d4911
何雨洋 <13598752812@163.com>
2025-09-05 21:36:52 +0800

    新增地图边界数据脚本

---
+++ Assets/Scripts/Data/MapBoundConfigData.cs
@@ -0,0 +1,14 @@
+﻿namespace Data
+{
+    /// <summary>
+    /// 地图的配置数据，目前暂时由 GameManager 创建并分发
+    /// </summary>
+    public class MapBoundConfigData
+    {
+        public float leftSpawnX;     // 左侧重生点X坐标
+        public float rightSpawnX;    // 右侧重生点X坐标
+        public float topBoundary;    // 垂直空间顶部
+        public float bottomBoundary; // 垂直空间底部
+        
+    }
+}
