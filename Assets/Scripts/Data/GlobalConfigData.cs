4c86439f53f0817c47e21c3db4c1f51b55059506
何雨洋 <13598752812@163.com>
2025-09-05 21:39:41 +0800

    新增全局配置数据SO脚本以及配置文件

---
+++ Assets/Scripts/Data/GlobalConfigData.cs
@@ -0,0 +1,27 @@
+﻿using Core.Collections;
+using Data.FishData;
+using Data.FishingComponent;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Data
+{
+    [CreateAssetMenu(fileName = "GlobalConfigData", menuName = "FishingGame/GlobalConfig")]
+    public class GlobalConfigData : ScriptableObject
+    {
+        [Header("输入配置文件")]
+        public InputActionAsset inputActionAsset;
+    
+        [Header("鱼竿配置")]
+        public FishRodConfig fishRodConfig;
+    
+        [Header("鱼钩配置")]
+        public GameObject fishHookPrefab;
+
+
+        [Header("局内配置")] 
+        public Vector2 fishHookStartPos;
+        public int fishCount;
+        public SerializableDictionary<EFishType,BaseFishData> fishDataDict;
+    }
+}
