291ac1a7797ed16eb6749e18b9e621e863d6ff2c
何雨洋 <13598752812@163.com>
2025-09-16 23:13:27 +0800

    增加注释

--- Assets/Scripts/Core/Runtime/EntityManager.cs
+++ Assets/Scripts/Core/Runtime/EntityManager.cs
@@ -18,13 +18,22 @@ namespace Core.Runtime
         NullEntity,             // 实体为空
         DuplicateId             // 重复的实体Id
     }
-    public interface IEntityFactory<TDataKey, TData, TEntity,TEntityKey>
+    /// <summary>
+    /// 实体工厂接口
+    /// </summary>
+    /// <typeparam name="TDataKey">数据类Id的数据类型</typeparam>
+    /// <typeparam name="TData">数据类的类型</typeparam>
+    /// <typeparam name="TEntity">实体类型</typeparam>
+    /// <typeparam name="TEntityKey">实体Id的数据类型</typeparam>
+    public interface IEntityFactory<in TDataKey, in TData, TEntity, out TEntityKey>
     {
         TEntity Create(TDataKey key, TData data, object context = null);
         void Destroy(TEntity entity);
         void Update(TEntity entity);
         TEntityKey GetId(TEntity entity);
     }
+    
+    
     /// <summary>
     /// 通用基类实体管理器，兼容 Mono和非Mono对象
     /// </summary>
